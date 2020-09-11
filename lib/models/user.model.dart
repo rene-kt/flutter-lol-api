f399681cec0033667db66ef3d67c64dbce1a4ef2
Renê <renejr.arraes286@gmail.com>
2020-09-11 13:19:25 -0300

    creating models

---
+++ lib/models/user.model.dart
@@ -0,0 +1,9 @@
+import 'package:FlutterGifGallery/models/champion.model.dart';
+
+class User {
+  String id;
+  String summonerName;
+  int profileIconId;
+  int summonerLevel;
+  List<Champion> championsMasteries;
+}
