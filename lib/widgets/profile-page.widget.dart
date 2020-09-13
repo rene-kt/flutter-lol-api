ea621ebed6186760d9e83959b8d5a4bf5274614b
Renê <renejr.arraes286@gmail.com>
2020-09-13 10:04:02 -0300

    creating profile page

--- lib/widgets/profile-page.widget.dart
+++ lib/widgets/profile-page.widget.dart
@@ -1,29 +1,139 @@
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
-  Widget returnAppBar() {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("SUMMONER NAME Q EU PEGO DO CONTROLLER TEXT"),
-        backgroundColor: Colors.black,
-      ),
-    );
-  }
-
-  Widget returnCircleProfileIcon() {
-    return CircleAvatar(
-      radius: 55,
-      backgroundColor: Color(0xffFDCF09),
-      child: CircleAvatar(
-        radius: 50,
-        backgroundImage: AssetImage('images/batman.jpg'),
-        //pegar imagem da API
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Column(children: <Widget>[
+      Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.redAccent, Colors.pinkAccent])),
+          child: Container(
+            width: double.infinity,
+            height: 350.0,
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      "https://www.rd.com/wp-content/uploads/2017/09/01-shutterstock_476340928-Irina-Bg.jpg",
+                      //PROFILE ICON DA API
+                    ),
+                    radius: 50.0,
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    "Alice James",
+                    //NOME DO INVOCADOR
+                    style: TextStyle(
+                      fontSize: 22.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Card(
+                    margin:
+                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+                    clipBehavior: Clip.antiAlias,
+                    color: Colors.white,
+                    elevation: 5.0,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8.0, vertical: 22.0),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                Text(
+                                  "LEVEL",
+                                  style: TextStyle(
+                                    color: Colors.redAccent,
+                                    fontSize: 22.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 5.0,
+                                ),
+                                Text(
+                                  "5200",
+                                  //SUMMONER LEVEL VINDO DA API
+                                  style: TextStyle(
+                                    fontSize: 20.0,
+                                    color: Colors.pinkAccent,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                Text(
+                                  "RANK",
+                                  style: TextStyle(
+                                    color: Colors.redAccent,
+                                    fontSize: 22.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 5.0,
+                                ),
+                                Text(
+                                  "28.5K",
+                                  //RANKED VINDO DA API: OURO IV, 15 pdl
+                                  style: TextStyle(
+                                    fontSize: 20.0,
+                                    color: Colors.pinkAccent,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                Text(
+                                  "W/R",
+                                  style: TextStyle(
+                                    color: Colors.redAccent,
+                                    fontSize: 22.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 5.0,
+                                ),
+                                Text(
+                                  "1300",
+                                  //WR, WINS/LOSE
+                                  style: TextStyle(
+                                    fontSize: 20.0,
+                                    color: Colors.pinkAccent,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ))
+    ]));
   }
 }
