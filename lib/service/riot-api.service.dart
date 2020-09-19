58cc5328fe3fa6e7b8b3268bd85b9541b72063a9
Renê <renejr.arraes286@gmail.com>
2020-09-19 18:31:48 -0300

    reseting to an old commit

--- lib/service/riot-api.service.dart
+++ lib/service/riot-api.service.dart
@@ -1,6 +1,6 @@
 class RiotApi {
   static const String base_url = "https://br1.api.riotgames.com/lol/";
-  static const String key = "RGAPI-3732eed7-0979-4172-9155-37b2339f362a";
+  static const String key = "RGAPI-1935bb37-65ab-46ab-9733-b5b0737112da";
   static const String summonerIdUrl = "summoner/v4/summoners/by-name/";
   static const String championsUrl =
       "champion-mastery/v4/champion-masteries/by-summoner/";
